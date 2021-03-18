5c2c1164cb0263b770c273a372370aa04208033b
Nick Handy <nichandy@gmail.com>
2021-03-18 01:54:43 -0700

    remove nanoid redundancy

--- src/components/deckList.jsx
+++ src/components/deckList.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 import DeckListItem from './deckListItem';
 
 const DeckList = ({ decks }) => {
-  const [id] = React.useState(nanoid);
   return (
     <div className="container">
       {decks.map(({ node }) => (
-        <div className="columns is-centered is-full" key={id}>
+        <div className="columns is-centered is-full" key={node.info.id}>
           <DeckListItem
             title={node.info.title}
             date={node.info.date}
